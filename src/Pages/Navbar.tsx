3b452d78588d4414867956726abea784780f8dc8
aaron <zwekokotun99@gmail.com>
2024-10-28 13:51:33 +0700

    feat:use clsx instead string itaporation

--- src/Pages/Navbar.tsx
+++ src/Pages/Navbar.tsx
@@ -2,17 +2,30 @@ import { useState } from "react"
 import {Input} from "../components/Input"
 import {Button} from "../components/Button"
 import {Avatar, AvatarImage, AvatarFallback} from "../components/Avatar"
-import Icon from "../components/Icon"
+import { AiOutlineBars, AiOutlineClose } from 'react-icons/ai'
+import { MdOutlineMail } from 'react-icons/md';
+import { IoNotificationsOutline, IoGlobe } from 'react-icons/io5';
+import clsx from 'clsx'
 
 const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false)
 
   const transition = "transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-red-600 duration-300 cursor-pointer"
+  const transitionClasses = transition.split(' ')
 
   const toggleMenu = () => {
     setIsOpen(!isOpen)
   }
+
+  const tabs = [
+    { href: "#", label: "Home" },
+    { href: "#", label: "Docs" },
+    { href: "#", label: "Inbox" },
+    { href: "#", label: "Notifications" },
+    { href: "#", label: "Profile" },
+    { href: "#", label: "Installations" },
+  ]
 
   return (
     <nav className="fixed top-0 w-full py-6 border-b-2 bg-white z-10">
@@ -28,17 +41,17 @@ const Navbar = () => {
           {/* Hamburger Icon */}
           <div className="md:hidden">
             <button onClick={toggleMenu} className="text-red-700 focus:outline-none">
-                {isOpen ? (<Icon type="xmark" className="w-6 h-6"/>) : (<Icon type="bars3" className="w-6 h-6"/>)}
+                {isOpen ? (<AiOutlineClose />) : (<AiOutlineBars />)}
             </button>
           </div>
 
           {/* Right Side */}
           <div className="hidden md:flex flex-row gap-10 items-center">
-            <Icon type="envelope" className={`${transition} cursor-pointer`}/>
-            <Icon type="bellalert" className={`${transition} cursor-pointer`}/>
+            <MdOutlineMail type="envelope" className={clsx(transitionClasses, 'cursor-pointer')}/>
+            <IoNotificationsOutline type="bellalert" className={clsx(transitionClasses, 'cursor-pointer')}/>
 
             <h1 className="text-xl text-bold cursor-pointer transition ease-in-out delay-150 hover:text-red-700">Orders</h1>
-            <Avatar size="md" className={`${transition}`}>
+            <Avatar size="md" className={clsx(transition)}>
               <AvatarImage className="h-12 w-12 rounded-full cursor-pointer" src="https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D"/>
               <AvatarFallback>
                 A
@@ -61,7 +74,7 @@ const Navbar = () => {
             <div className="mt-4 flex flex-col space-y-2 px-10 pb-4">
                <a href="#" className ="text-red-700 text-lg font-semibold">General</a>
                <a href="#" className="hover:text-red-500 transition-colors duration-300">Logout</a>
-               <a href="#" className="hover:text-red-500 transition-colors duration-300">English <Icon type="globealt" className="inline"/></a>
+               <a href="#" className="hover:text-red-500 transition-colors duration-300">English <IoGlobe type="globealt" className="inline"/></a>
                <a href="#" className="hover:text-red-500 transition-colors duration-300">Settings</a>
                <a href="#" className="hover:text-red-500 transition-colors duration-300">Billing & Payments</a>
             </div>
